2827a140863c1f77ae16f0c166b50831f452c718
Tom Österlund <tom.osterlund1@gmail.com>
2022-06-01 20:35:58 +0200

    comment in date picker export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export { default as Qalendar } from './Qalendar.vue'
-// export { default as DatePicker} from "./components/header/DatePicker.vue";
+import Qalendar from "./Qalendar.vue";
+import DatePicker from "./components/header/DatePicker.vue";
+
+export { Qalendar, DatePicker }
