a229425a6bf3af29ceb66569c1dd4e69c72479eb
Tom Österlund <tom.osterlund1@gmail.com>
2022-07-07 00:34:50 +0200

    docs: document usage of html in events

---
+++ docs/__data__/qalendar-demo-2.ts
@@ -0,0 +1,398 @@
+export default [{
+  "title": "Meeting with Dora",
+  "with": "Albert Einstein",
+  "time": {"start": "2022-01-01 04:52", "end": "2022-01-01 05:37"},
+  "color": "green",
+  "isEditable": true,
+  "id": "de471c78cb5c",
+  "description": "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome."
+}, {
+  "title": "Advanced algebra",
+  "with": "Pheobe Buffay",
+  "time": {"start": "2022-01-02 20:05", "end": "2022-01-02 21:35"},
+  "colorScheme": "sports",
+  "isEditable": true,
+  "id": "6d3c0980a5cf",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+}, {
+  "title": "Break",
+  "with": "Marshall Eriksen",
+  "time": {"start": "2022-01-02 22:10", "end": "2022-01-02 22:55"},
+  "colorScheme": "meetings",
+  "isEditable": true,
+  "id": "9f1b209982f1",
+  "location": "Zoom"
+}, {
+  "title": "English essay deadline",
+  "with": "Ross Geller",
+  "time": {"start": "2022-01-03 02:05", "end": "2022-01-03 02:55"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "9c85ae24b7f7",
+  "description": "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome.",
+  "location": "120 Center Str, 95872 Saint Cloud, MN"
+}, {
+  "title": "Advanced algebra",
+  "with": "Max Mustermann",
+  "time": {"start": "2022-01-03 17:30", "end": "2022-01-03 18:30"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "fa558428df3c",
+  "location": "Room 3002"
+}, {
+  "title": "Meeting with Dora",
+  "with": "Ross Geller",
+  "time": {"start": "2022-01-04 09:05", "end": "2022-01-04 10:35"},
+  "color": "green",
+  "isEditable": true,
+  "id": "dee23be3fd33"
+}, {
+  "title": "Break",
+  "with": "Marshall Eriksen",
+  "time": {"start": "2022-01-05 16:05", "end": "2022-01-05 16:35"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "2302dc2fa49a",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!",
+  "location": "29 40th Street, 12345 Chikinawa"
+}, {
+  "title": "Meeting: John w. parents",
+  "with": "Monica Geller",
+  "time": {"start": "2022-01-06 04:35", "end": "2022-01-06 06:05"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "ce58cd392a8a"
+}, {
+  "title": "Outdoor sports",
+  "with": "Barney Stinson",
+  "time": {"start": "2022-01-06 12:50", "end": "2022-01-06 13:40"},
+  "color": "green",
+  "isEditable": true,
+  "id": "4293bf845d98"
+}, {
+  "title": "Break",
+  "with": "John Doe",
+  "time": {"start": "2022-01-06 16:20", "end": "2022-01-06 17:50"},
+  "color": "green",
+  "isEditable": true,
+  "id": "9af592cb2f05"
+}, {
+  "title": "English essay deadline",
+  "with": "Tom Österlund",
+  "time": {"start": "2022-01-07 04:50", "end": "2022-01-07 05:50"},
+  "color": "green",
+  "isEditable": true,
+  "id": "accc6cd8e647",
+  "description": "Pick up the dry cleaning. 234 Center Str.",
+  "location": "The grand lecture hall"
+}, {
+  "title": "DEADLINE - english essay",
+  "with": "Lily Aldrin",
+  "time": {"start": "2022-01-07 07:14", "end": "2022-01-07 07:59"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "2d4a22195468",
+  "location": "Zoom"
+}, {
+  "title": "Break",
+  "with": "Monica Geller",
+  "time": {"start": "2022-01-07 13:35", "end": "2022-01-07 14:35"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "208c0c6a35ff",
+  "description": "Sports attire required"
+}, {
+  "title": "Outdoor sports",
+  "with": "Chandler Bing",
+  "time": {"start": "2022-01-08 00:30", "end": "2022-01-08 02:30"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "f7745909c48e",
+  "description": "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome."
+}, {
+  "title": "Introduction to algebra",
+  "with": "John Doe",
+  "time": {"start": "2022-01-08 23:47", "end": "2022-01-09 01:17"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "59bcdd2b6ecf",
+  "description": "Read textbook p. 70-72"
+}, {
+  "title": "Meeting with Dora",
+  "with": "Marshall Eriksen",
+  "time": {"start": "2022-01-11 19:35", "end": "2022-01-11 20:35"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "712c65449155",
+  "location": "Zoom"
+}, {
+  "title": "DEADLINE - english essay",
+  "with": "Martin Gruber",
+  "time": {"start": "2022-01-12 02:52", "end": "2022-01-12 03:52"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "a5164269d8f7",
+  "description": "Think of me as Yoda. Only instead of being little and green, I wear suites and I'm awesome."
+}, {
+  "title": "Advanced algebra",
+  "with": "Joey Tribiani",
+  "time": {"start": "2022-01-13 03:25", "end": "2022-01-13 05:25"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "9368927cd7d7",
+  "location": "Fejkvägen 90, 234 91 Solsidan"
+}, {
+  "title": "Advanced algebra",
+  "with": "Joey Tribiani",
+  "time": {"start": "2022-01-13 16:25", "end": "2022-01-13 17:55"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "c1a93c487de3"
+}, {
+  "title": "Break",
+  "with": "Lily Aldrin",
+  "time": {"start": "2022-01-14 02:15", "end": "2022-01-14 03:45"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "f3b541d4ef0a",
+  "description": "Sports attire required",
+  "location": "The green lab"
+}, {
+  "title": "Introduction to algebra",
+  "with": "Marshall Eriksen",
+  "time": {"start": "2022-01-14 15:14", "end": "2022-01-14 17:14"},
+  "color": "green",
+  "isEditable": true,
+  "id": "0f82a9f8e028",
+  "location": "Fejkvägen 90, 234 91 Solsidan"
+}, {
+  "title": "Break",
+  "with": "Chandler Bing",
+  "time": {"start": "2022-01-14 22:47", "end": "2022-01-14 23:32"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "0a4b8d3cddcc",
+  "location": "29 40th Street, 12345 Chikinawa"
+}, {
+  "title": "DEADLINE - english essay",
+  "with": "Max Mustermann",
+  "time": {"start": "2022-01-15 02:05", "end": "2022-01-15 03:35"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "993a5bcfc99a",
+  "location": "Zoom"
+}, {
+  "title": "Outdoor sports",
+  "with": "Martin Gruber",
+  "time": {"start": "2022-01-15 08:15", "end": "2022-01-15 10:15"},
+  "color": "green",
+  "isEditable": true,
+  "id": "389fbc90dc9a",
+  "location": "Room 3002"
+}, {
+  "title": "Introduction to algebra",
+  "with": "Janina Elisabeth Parker Larson",
+  "time": {"start": "2022-01-16 01:52", "end": "2022-01-16 03:52"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "c533444e095f",
+  "description": "Sports attire required"
+}, {
+  "title": "Call with Karo",
+  "with": "Erika Musterfrau",
+  "time": {"start": "2022-01-16 07:45", "end": "2022-01-16 08:45"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "d27a595e1887",
+  "location": "Room 3002"
+}, {
+  "title": "Advanced algebra",
+  "with": "Monica Geller",
+  "time": {"start": "2022-01-16 08:30", "end": "2022-01-16 10:30"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "4ed183aed6c1",
+  "description": "Pick up the dry cleaning. 234 Center Str.",
+  "location": "Zoom"
+}, {
+  "title": "Call with Karo",
+  "with": "John Doe",
+  "time": {"start": "2022-01-17 01:10", "end": "2022-01-17 02:40"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "7614245ce352"
+}, {
+  "title": "English essay deadline",
+  "with": "Janina Elisabeth Parker Larson",
+  "time": {"start": "2022-01-17 05:40", "end": "2022-01-17 07:10"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "26f255ce4e4f",
+  "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores assumenda corporis doloremque et expedita molestias necessitatibus quam quas temporibus veritatis. Deserunt excepturi illum nobis perferendis praesentium repudiandae saepe sapiente voluptatem!"
+}, {
+  "title": "Outdoor sports",
+  "with": "Pheobe Buffay",
+  "time": {"start": "2022-01-17 13:25", "end": "2022-01-17 14:25"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "43cb549863c6"
+}, {
+  "title": "Meeting: John w. parents",
+  "with": "John Doe",
+  "time": {"start": "2022-01-17 19:47", "end": "2022-01-17 21:17"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "0d65a06418ea",
+  "description": "Pick up the dry cleaning. 234 Center Str."
+}, {
+  "title": "Introduction to algebra",
+  "with": "Martin Gruber",
+  "time": {"start": "2022-01-17 23:00", "end": "2022-01-17 23:45"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "99a83625a3af",
+  "location": "The green lab"
+}, {
+  "title": "Break",
+  "with": "Elisabeth Gruber",
+  "time": {"start": "2022-01-18 12:55", "end": "2022-01-18 13:10"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "2aababcc1dcb",
+  "description": "Sports attire required",
+  "location": "Zoom"
+}, {
+  "title": "Advanced algebra",
+  "with": "Erika Musterfrau",
+  "time": {"start": "2022-01-19 01:15", "end": "2022-01-19 02:00"},
+  "color": "green",
+  "isEditable": true,
+  "id": "10d7978e5d3a",
+  "description": "Sports attire required",
+  "location": "The grand lecture hall"
+}, {
+  "title": "Introduction to algebra",
+  "with": "Max Mustermann",
+  "time": {"start": "2022-01-20 12:50", "end": "2022-01-20 14:20"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "33c04d6d4e77",
+  "location": "Zoom"
+}, {
+  "title": "Introduction to algebra",
+  "with": "Alejandro Montes Oca de Munós",
+  "time": {"start": "2022-01-21 06:40", "end": "2022-01-21 08:10"},
+  "color": "green",
+  "isEditable": true,
+  "id": "c3a64b41fcf8"
+}, {
+  "title": "Meeting: John w. parents",
+  "with": "Monica Geller",
+  "time": {"start": "2022-01-22 01:14", "end": "2022-01-22 02:44"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "371761255730",
+  "description": "Pick up the dry cleaning. 234 Center Str."
+}, {
+  "title": "Meeting: John w. parents",
+  "with": "Robin Scherbatsky",
+  "time": {"start": "2022-01-22 20:30", "end": "2022-01-22 21:30"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "7ef6b4e3ffd3",
+  "description": "Pick up the dry cleaning. 234 Center Str."
+}, {
+  "title": "Call with Karo",
+  "with": "Lily Aldrin",
+  "time": {"start": "2022-01-22 21:30", "end": "2022-01-22 22:30"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "b4c93a4f29b2",
+  "location": "120 Center Str, 95872 Saint Cloud, MN"
+}, {
+  "title": "Introduction to algebra",
+  "with": "Alejandro Montes Oca de Munós",
+  "time": {"start": "2022-01-23 14:00", "end": "2022-01-23 15:00"},
+  "color": "green",
+  "isEditable": true,
+  "id": "aa56751cb71c"
+}, {
+  "title": "Advanced algebra",
+  "with": "Rachel Greene",
+  "time": {"start": "2022-01-24 09:10", "end": "2022-01-24 10:40"},
+  "color": "green",
+  "isEditable": true,
+  "id": "e90df5c186b3"
+}, {
+  "title": "Meeting: John w. parents",
+  "with": "Lily Aldrin",
+  "time": {"start": "2022-01-26 00:52", "end": "2022-01-26 02:22"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "d13ab708dfa6",
+  "location": "The grand lecture hall"
+}, {
+  "title": "Break",
+  "with": "Tom Österlund",
+  "time": {"start": "2022-01-27 03:30", "end": "2022-01-27 05:00"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "707132f8c153",
+  "location": "The grand lecture hall"
+}, {
+  "title": "DEADLINE - english essay",
+  "with": "Martin Gruber",
+  "time": {"start": "2022-01-27 15:00", "end": "2022-01-27 15:45"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "e4a49a6fb185",
+  "description": "Pick up the dry cleaning. 234 Center Str."
+}, {
+  "title": "Introduction to algebra",
+  "with": "Erika Musterfrau",
+  "time": {"start": "2022-01-28 00:00", "end": "2022-01-28 02:00"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "dfd8a781aa32",
+  "description": "Pick up the dry cleaning. 234 Center Str.",
+  "location": "Room 3002"
+}, {
+  "title": "Outdoor sports",
+  "with": "Alejandro Montes Oca de Munós",
+  "time": {"start": "2022-01-28 11:00", "end": "2022-01-28 11:30"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "a2e3ba2ee7f7"
+}, {
+  "title": "Outdoor sports",
+  "with": "Albert Einstein",
+  "time": {"start": "2022-01-29 10:30", "end": "2022-01-29 12:30"},
+  "color": "yellow",
+  "isEditable": true,
+  "id": "30c12052bda0",
+  "location": "120 Center Str, 95872 Saint Cloud, MN"
+}, {
+  "title": "Outdoor sports",
+  "with": "Robin Scherbatsky",
+  "time": {"start": "2022-01-29 13:05", "end": "2022-01-29 15:05"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "4dd300524230",
+  "location": "29 40th Street, 12345 Chikinawa"
+}, {
+  "title": "Break",
+  "with": "Ted Mosby",
+  "time": {"start": "2022-01-30 02:47", "end": "2022-01-30 04:17"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "f734a778ee51",
+  "location": "The green lab"
+}, {
+  "title": "Break",
+  "with": "Martin Gruber",
+  "time": {"start": "2022-01-31 15:00", "end": "2022-01-31 16:30"},
+  "color": "blue",
+  "isEditable": true,
+  "id": "c73c186f002d",
+  "description": "Pick up the dry cleaning. 234 Center Str."
+}];
