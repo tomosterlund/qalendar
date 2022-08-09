0eb6e366e4c97a4cdaa8fff71dfee906867fbaa0
Tom Österlund <tom.osterlund1@gmail.com>
2022-08-09 08:41:20 +0200

    fix: roll back ts changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { DefineComponent } from 'vue'
+import Qalendar from './Qalendar.vue';
+import DatePicker from './components/header/DatePicker.vue';
 
-// TODO: Needs to be done in a more accurate and maintainable way https://github.com/tomosterlund/qalendar/projects/1#card-84848507
-export let Qalendar: DefineComponent<{ config: any, events: any }, { [key: string]: any }>
-
-export let DatePicker: DefineComponent<{}, {}>
+export { Qalendar, DatePicker };
