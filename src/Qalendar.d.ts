f6b71526ee4d50539213b8dd728ed17ffd2f5acd
Tom Österlund <tom.osterlund1@gmail.com>
2022-08-12 23:02:08 +0200

    feat: add declarations file for Qalendar component

---
+++ src/Qalendar.d.ts
@@ -0,0 +1,20 @@
+import Vue, { VNode } from 'vue';
+import type {eventInterface} from './typings/interfaces/event.interface';
+import type {configInterface} from './typings/config.interface';
+
+interface tolerantEventInterface extends eventInterface {
+  [key: string]: any;
+}
+
+// @ts-ignore
+declare class Qalendar extends Vue {
+  events: tolerantEventInterface[];
+  config: configInterface;
+
+  $slots: {
+    eventDialog: VNode[];
+    event: VNode[];
+  };
+}
+
+export default Qalendar;
