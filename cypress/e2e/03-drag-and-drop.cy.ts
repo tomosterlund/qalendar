e29fe1a45f1ae865d674ebdb6a63cb89506fd3f5
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2022-09-17 12:24:58 +0200

    Feature/change in quarter hours on dragging events (#68)
    * feat: allow quarters in dragging
    
    * feat: optimize drag behavior for quarter hours
    
    * chore: add automated test and build for pushes on master
    
    * chore: edit workflows
    
    * chore: edit workflows

--- cypress/e2e/03-drag-and-drop.cy.ts
+++ cypress/e2e/03-drag-and-drop.cy.ts
@@ -50,7 +50,7 @@ describe('DragAndDrop.vue', () => {
 
       cy.get(EVENT_SELECTOR).click()
       cy.get('.event-flyout__row.is-time').then($el => {
-        expect($el).to.have.text('June 10, 2022 ⋅ 11:00 PM - 12:20 AM')
+        expect($el).to.have.text('June 10, 2022 ⋅ 11:45 PM - 1:05 AM')
       })
     })
   });
