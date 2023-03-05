359d4cf8092aa6a46b7b72d756d5e887beb1dcf4
loicgeek <loic.ngou98@gmail.com>
2023-03-05 10:45:26 +0100

    fix: better test  description  for Month view

--- tests/unit/components/month/Month.test.ts
+++ tests/unit/components/month/Month.test.ts
@@ -8,9 +8,11 @@ describe("Month.vue", () => {
   const wrapper = mount(Month, {
     props: {
       eventsProp: [],
-
+      
       time: new Time("monday", "de-DE"),
-      config: {},
+      config: {
+       
+      },
       period: {
         start: new Date(2023, 3 - 1, 1),
         end: new Date(2023, 3 - 1, 31),
@@ -19,20 +21,21 @@ describe("Month.vue", () => {
     },
   });
 
-  test("Displaying the correct number of weeks and days", async () => {
+  test("given a month, display the correct number of weeks and days", async () => {
     wrapper.vm.setMonth();
     await nextTick();
+
+    const expectedCalendarWeeksCount = 5;
     const calendarWeeks = wrapper.findAll(".calendar-month__week");
-    expect(calendarWeeks).toHaveLength(6);
+    expect(calendarWeeks).toHaveLength(5);
     const calendarWeekdays = wrapper.findAll(".calendar-month__weekday");
-    expect(calendarWeekdays).toHaveLength(42);
+    expect(calendarWeekdays).toHaveLength(expectedCalendarWeeksCount *7);
   });
-  test("Displaying the correct number of leading and trailing day", async () => {
+  test("given a month, the calendar should  display  the correct number of leading and trailing day", async () => {
     wrapper.vm.setMonth();
     await nextTick();
     const trailingAndLeadingDays = wrapper.findAll(".trailing-or-leading");
     console.log(trailingAndLeadingDays);
-    
     expect(trailingAndLeadingDays).toHaveLength(4);
   });
 });
