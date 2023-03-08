6996f432f564b86b08bf654143fd3c8f32e8b65c
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2023-03-08 21:20:43 +0100

    docs: add new landing page and design (#133)
    * docs: upgrade to vitepress 1.alpha and create new landing page
    
    * chore: upgrade fortawesome dependencies
    
    * fix: paths to assets for docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,10 +4,16 @@ export default {
   description: "Vue 3 calendar component",
   themeConfig: {
     nav: [
-      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide" },
       { text: "v1 to v2", link: "/migration" },
       { text: "Roadmap", link: "https://github.com/tomosterlund/qalendar/projects/1" },
       { text: "Github", link: "https://github.com/tomosterlund/qalendar" },
     ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2022-present Tom Österlund'
+    }
   },
+  outline: [2, 3],
+  appearance: false,
 };
