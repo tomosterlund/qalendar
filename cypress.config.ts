0d68e8b36b3a4483218ef62979283b9be006892e
Tom Österlund <tom.osterlund1@gmail.com>
2023-06-15 21:08:47 +0200

    test: fix folder structure for linux tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,27 @@
 import { defineConfig } from 'cypress';
 import getCompareSnapshotsPlugin from "cypress-visual-regression/dist/plugin";
+import { platform } from 'os';
+
+const getPlatformForCypressSnapshots = () => {
+  const isMac = platform() === 'darwin';
+  const isLinux = platform() === 'linux';
+  if (!isMac && !isLinux) {
+    throw new Error('Unsupported platform for cypress snapshots');
+  }
+
+  return isMac ? 'mac' : 'linux';
+}
+
+const platformForCypressSnapshots = getPlatformForCypressSnapshots();
 
 export default defineConfig({
+  video: false,
   env: {
-    screenshotsFolder: './cypress/snapshots/',
+    SNAPSHOT_BASE_DIRECTORY: `./cypress/snapshots/${platformForCypressSnapshots}/base`,
+    SNAPSHOT_DIFF_DIRECTORY: `./cypress/snapshots/${platformForCypressSnapshots}/diff`,
+    ALWAYS_GENERATE_DIFF: 'true',
+    screenshotsFolder: `./cypress/snapshots/${platformForCypressSnapshots}/actual`,
     trashAssetsBeforeRuns: true,
-    video: false,
   },
   e2e: {
     baseUrl: 'http://localhost:5173',
