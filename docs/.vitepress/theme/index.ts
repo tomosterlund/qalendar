cf62c8a9e1b106dbcde11e4d596c051959a18011
Tom Österlund <63011648+tomosterlund@users.noreply.github.com>
2023-06-23 08:40:20 +0200

    docs: fix build for docs (#183)

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,26 +2,29 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
 
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    const target = mutation.target as Element
+if (typeof window === 'object') {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const target = mutation.target as Element
 
-    if (mutation.attributeName === 'class') {
-      const newValue = target.getAttribute(mutation.attributeName)
-      if (newValue.includes('dark')) {
-        target.setAttribute('style', 'color-scheme: dark')
-      } else {
-        target.removeAttribute('style')
+      if (mutation.attributeName === 'class') {
+        const newValue = target.getAttribute(mutation.attributeName)
+        if (newValue.includes('dark')) {
+          target.setAttribute('style', 'color-scheme: dark')
+        } else {
+          target.removeAttribute('style')
+        }
       }
-    }
+    })
   })
-})
 
-observer.observe(document.documentElement, {
-  attributes: true,
-  attributeFilter: ['class'],
-  childList: false,
-});
+  observer.observe(document.documentElement, {
+    attributes: true,
+    attributeFilter: ['class'],
+    childList: false,
+  });
+}
+
 
 
 export default DefaultTheme
