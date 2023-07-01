1e474091cde65244d1ae01f3e6b64d9259c29c12
Tom Österlund <tom.osterlund1@gmail.com>
2023-07-01 14:29:56 +0200

    test: add unit tests for Header

--- tests/unit/components/header/Header.test.ts
+++ tests/unit/components/header/Header.test.ts
@@ -1,27 +1,36 @@
 import { mount } from "@vue/test-utils";
 import Header from "../../../../src/components/header/Header.vue";
-import { describe, expect, test } from "vitest";
-import Time from "../../../../src/helpers/Time";
+import { describe, expect, it, vi } from "vitest";
+import Time, { WEEK_START_DAY } from "../../../../src/helpers/Time";
 import { nextTick } from "vue";
 
 describe("Header.vue", () => {
-  let wrapper = mount(Header, {
-    props: {
-      time: new Time("monday", "en-US"),
-      period: {
-        selectedDate: new Date(),
-        start: new Date(),
-        end: new Date(),
-      },
+  const defaultProps = {
+    time: new Time(WEEK_START_DAY.MONDAY, "en-US"),
+    period: {
+      selectedDate: new Date(),
+      start: new Date(),
+      end: new Date(),
     },
-  });
+  };
 
-  test("viewing the month(s) name", () => {
+  const factory = (props = {}) => {
+    return  mount(Header, {
+      props: {
+        ...defaultProps,
+        ...props,
+      }
+    });
+  }
+
+  it("should view the month(s) name", () => {
+    const wrapper = factory();
     const monthName = wrapper.find(".calendar-header__period-name");
     expect(monthName.text()).toBeDefined();
   });
 
-  test("Emitting event for changing to month mode", async () => {
+  it("should emit event for changing to month mode", async () => {
+    const wrapper = factory();
     await wrapper.find(".calendar-header__mode-value").trigger("click");
     await nextTick();
     await wrapper.find(".is-month-mode").trigger("click");
@@ -31,17 +40,8 @@ describe("Header.vue", () => {
     expect(changeModeEvent[0]).toEqual(["month"]);
   });
 
-  test("Emitting event for changing to day mode", async () => {
-    wrapper = mount(Header, {
-      props: {
-        time: new Time("monday", "en-US"),
-        period: {
-          selectedDate: new Date(),
-          start: new Date(),
-          end: new Date(),
-        },
-      },
-    });
+  it("should emit event for changing to day mode", async () => {
+    const wrapper = factory();
     await wrapper.find(".calendar-header__mode-value").trigger("click");
     await wrapper.find(".is-day-mode").trigger("click");
     const changeModeEvent = wrapper.emitted("change-mode");
@@ -50,17 +50,8 @@ describe("Header.vue", () => {
     expect(changeModeEvent[0]).toEqual(["day"]);
   });
 
-  test("Emitting event for changing to week mode", async () => {
-    wrapper = mount(Header, {
-      props: {
-        time: new Time("monday", "en-US"),
-        period: {
-          selectedDate: new Date(),
-          start: new Date(),
-          end: new Date(),
-        },
-      },
-    });
+  it("should emit event for changing to week mode", async () => {
+    const wrapper = factory();
     await wrapper.find(".calendar-header__mode-value").trigger("click");
     await wrapper.find(".is-week-mode").trigger("click");
     const changeModeEvent = wrapper.emitted("change-mode");
@@ -69,21 +60,62 @@ describe("Header.vue", () => {
     expect(changeModeEvent[0]).toEqual(["week"]);
   });
 
-  test('Disabling week & month mode, thus hiding the mode select', () => {
-    wrapper = mount(Header, {
-      props: {
-        time: new Time("monday", "en-US"),
-        period: {
-          selectedDate: new Date(),
-          start: new Date(),
-          end: new Date(),
-        },
-        config: {
-          disableModes: ['week', 'month'],
-        }
+  it('should disable week & month mode, thus hiding the mode select', () => {
+    const wrapper = factory({
+      config: {
+        disableModes: ['week', 'month'],
       },
-    });
+    })
 
     expect(() => wrapper.get('.calendar-header__mode-picker')).toThrow();
   })
+
+  it('should emit event "updated-period" when DatePicker sends @updated event', () => {
+    const wrapper = factory();
+    const expectedEventPayload = {
+      start: new Date(),
+      end: new Date(),
+      selectedDate: new Date(),
+    };
+    wrapper.findComponent({ name: "DatePicker" }).vm.$emit("updated", expectedEventPayload);
+    const updatePeriodEvent = wrapper.emitted("updated-period");
+
+    if (!updatePeriodEvent) throw new Error("no event");
+    expect(updatePeriodEvent[0]).toEqual([expectedEventPayload]);
+  })
+
+  it('should update current period when DatePicker sends @updated event', () => {
+    const wrapper = factory();
+    const expectedEventPayload = {
+      start: new Date(),
+      end: new Date(),
+      selectedDate: new Date(),
+    };
+    wrapper.findComponent({ name: "DatePicker" }).vm.$emit("updated", expectedEventPayload);
+    expect(wrapper.vm.currentPeriod).toEqual(expectedEventPayload);
+  })
+
+  it('should update period forwards in DatePicker, when clicking right chevron', () => {
+    const wrapper = factory();
+    const DatePicker = wrapper.findComponent({ name: "DatePicker" });
+    const goToPeriodSpy = vi.spyOn(DatePicker.vm, 'goToPeriod');
+
+    wrapper.find('.calendar-header__chevron-arrow-right').trigger('click');
+
+    expect(goToPeriodSpy).toHaveBeenCalledWith('next');
+  })
+
+  it('should update period backwards in DatePicker, when clicking left chevron', () => {
+    const wrapper = factory();
+    const DatePicker = wrapper.findComponent({ name: "DatePicker" });
+    const goToPeriodSpy = vi.spyOn(DatePicker.vm, 'goToPeriod');
+
+    wrapper.find('.calendar-header__chevron-arrow-left').trigger('click');
+
+    expect(goToPeriodSpy).toHaveBeenCalledWith('previous');
+  })
+
+  it.todo('should display period name for week mode')
+
+  it.todo('should display period name for month mode')
 });
