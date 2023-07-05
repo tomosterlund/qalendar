7d4a52963b1ab5df07ba0e9bc92709dac919927e
Tom Österlund <tom.osterlund1@gmail.com>
2023-07-05 21:17:09 +0200

    test: add unit tests for month/Event.vue

--- tests/unit/components/month/Event.test.ts
+++ tests/unit/components/month/Event.test.ts
@@ -1,44 +1,187 @@
-import { mount } from "@vue/test-utils";
-import { describe, test, expect } from "vitest";
+import {mount} from "@vue/test-utils";
+import {describe, expect, it, vi} from "vitest";
 import Event from "../../../../src/components/month/Event.vue";
-import Time from "../../../../src/helpers/Time";
+import Time, {WEEK_START_DAY} from "../../../../src/helpers/Time";
 import {dayInterface} from '../../../../src/typings/interfaces/day.interface';
 import unidecode from 'unidecode';
+import {dataRefFrom} from "../../../vitest-setup";
+import {EventBuilder} from "../../../../src/models/Event";
+import {modeType} from "../../../../src/typings/types";
+import {eventInterface} from "../../../../src/typings/interfaces/event.interface";
+import {EVENT_COLORS} from "../../../../src/constants";
+
+const EVENT_TIME = ".calendar-month__event-time";
 
 describe("Event.vue", () => {
-  let wrapper = mount(Event, {
+  const defaultOptions = {
     props: {
-      time: new Time("sunday", "en-US"),
+      time: new Time(WEEK_START_DAY.SUNDAY, "en-US"),
       calendarEvent: {
         title: "Foo",
         time: { start: "2022-05-22 00:00", end: "2022-05-22 01:00" },
         id: "123",
-      },
-      config: {},
-      day: { dayName: 'monday', events: [], dateTimeString: '2022-01-01' } as dayInterface
+      } as eventInterface,
+      config: { style: { colorSchemes: {} } },
+      day: { dayName: 'monday', events: [], dateTimeString: '2022-01-01' } as dayInterface,
     },
-  });
+  }
 
-  test("Displaying the event title", () => {
+  it("should displaying the event title", () => {
+    const wrapper = mount(Event, defaultOptions);
     const title = wrapper.find(".calendar-month__event-title");
     expect(title.text()).toBe("Foo");
   });
 
-  test("Displaying the time", () => {
-    const time = wrapper.find(".calendar-month__event-time");
+  it("should display the time", () => {
+    const wrapper = mount(Event, defaultOptions);
+    const time = wrapper.find(EVENT_TIME);
     expect(
       unidecode(time.text())
     ).toBe("12:00 AM");
   });
 
-  test('Emitting event "event-was-clicked"', async () => {
+  it('should not find any event time if calendarEvent is a full day event', () => {
+    const wrapper = mount(Event, {
+      ...defaultOptions,
+      props: {
+        ...defaultOptions.props,
+        calendarEvent: new EventBuilder({
+          start: '2022-05-22',
+          end: '2022-05-22'
+        }).build()
+      }
+    })
+    expect(wrapper.find(EVENT_TIME).exists()).toBeFalsy()
+  })
+
+  it('should emit event "event-was-clicked"', async () => {
+    const wrapper = mount(Event, defaultOptions)
     const eventElement = wrapper.find('.calendar-month__event')
     await eventElement.trigger('click')
     const wasClickedEvent = wrapper.emitted('event-was-clicked')
 
-    // @ts-ignore
     expect(wasClickedEvent[0][0]).toHaveProperty('clickedEvent')
-    // @ts-ignore
     expect(wasClickedEvent[0][0]).toHaveProperty('eventElement')
+  })
+
+  it('should render default event', () => {
+    const wrapper = mount(Event, defaultOptions)
+    expect(wrapper.find(dataRefFrom('default-event')).exists()).toBeTruthy()
+  })
+
+  it.each([
+    // [isCustomProperty, result]
+    [true,              true],
+    [['month', 'week'], true],
+    [['week'],          false]
+  ])('should render event with custom template if isCustom is true for month mode', (
+    isCustomProperty,
+    result
+  ) => {
+    const options = {
+      ...defaultOptions,
+      props: {
+        ...defaultOptions.props,
+        calendarEvent: new EventBuilder({
+          start: '2022-05-22 00:00',
+          end: '2022-05-22 01:00',
+        })
+          .withIsCustom(isCustomProperty as boolean | modeType[])
+          .build(),
+      }
+    }
+    const wrapper = mount(Event, options)
+    expect(wrapper.find(dataRefFrom('custom-event')).exists()).toBe(result)
+  })
+
+  it('should not find element with class is-draggable because event is not editable', () => {
+    const wrapper = mount(Event, defaultOptions)
+    expect(wrapper.find('.is-draggable').exists()).toBeFalsy()
+  })
+
+  it('should not find element with class is-draggable because DnD is disabled', () => {
+    const options = { ...defaultOptions }
+    options.props.calendarEvent = new EventBuilder({
+      start: '2022-05-22 00:00',
+      end: '2022-05-22 01:00',
+    })
+      .withIsEditable(true)
+      .withDisableDnD(['month'])
+      .build()
+    const wrapper = mount(Event, options)
+    expect(wrapper.find('.is-draggable').exists()).toBeFalsy()
+  })
+
+  it('should find element with class is-draggable', () => {
+    const options = { ...defaultOptions }
+    options.props.calendarEvent = new EventBuilder({
+      start: '2022-05-22 00:00',
+      end: '2022-05-22 01:00',
+    })
+      .withIsEditable(true)
+      .withDisableDnD(['week'])
+      .build()
+    const wrapper = mount(Event, options)
+    expect(wrapper.find('.is-draggable').exists()).toBeTruthy()
+  })
+
+  it('should set background color to blue per default', () => {
+    const wrapper = mount(Event, defaultOptions)
+    expect(wrapper.vm.eventBackgroundColor).toBe(EVENT_COLORS.blue)
+  })
+
+  it('should set background color to yellow', () => {
+    const options = { ...defaultOptions }
+    options.props.calendarEvent = new EventBuilder({
+      start: '2022-05-22 00:00',
+      end: '2022-05-22 01:00',
+    })
+      .withColor('yellow')
+      .build()
+    const wrapper = mount(Event, options)
+    expect(wrapper.vm.eventBackgroundColor).toBe(EVENT_COLORS.yellow)
+  })
+
+  it('should set background color to custom color scheme', () => {
+    const options = { ...defaultOptions }
+    const expectedBackgroundColor = '#ff0000';
+    options.props.config.style.colorSchemes = {
+      women: {
+        backgroundColor: expectedBackgroundColor,
+        color: '#ffa',
+      }
+    }
+    options.props.calendarEvent = new EventBuilder({
+      start: '2022-05-22 00:00',
+      end: '2022-05-22 01:00',
+    })
+      .withColorScheme('women')
+      .build()
+    const wrapper = mount(Event, options)
+    expect(wrapper.vm.eventBackgroundColor).toBe(expectedBackgroundColor)
+  })
+
+  it('should set data onto the data transfer of a drag event', async () => {
+    const calendarEvent = new EventBuilder({
+      start: '2022-05-22 00:00',
+      end: '2022-05-22 01:00',
+    }).build()
+    const options = { ...defaultOptions }
+    options.props.calendarEvent = calendarEvent
+    const wrapper = mount(Event, options)
+    const dragEvent = {
+      dataTransfer: {
+        effectAllowed: '',
+        setData: vi.fn(),
+      }
+    }
+
+    await wrapper.find('.is-event').trigger('dragstart', dragEvent)
+
+    expect(dragEvent.dataTransfer.effectAllowed).toBe('move')
+    expect(dragEvent.dataTransfer.setData).toHaveBeenCalledWith(
+      'json',
+      JSON.stringify(calendarEvent)
+    )
   })
 });
--- tests/vitest-setup.ts
+++ tests/vitest-setup.ts
@@ -1,1 +1,3 @@
 export const mountComponent = (mount: Function, component: any) => (options: object|unknown) => mount(component, options)
+
+export const dataRefFrom = (dataRefText) => `[data-ref="${dataRefText}"]`
